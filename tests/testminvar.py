52a13fbc58cd0ca52da5cc8164b65f7628073769
ozagordi <osvaldo.zagordi+git@gmail.com>
2015-05-04 17:20:47 +0200

    Triggering

--- tests/testminvar.py
+++ tests/testminvar.py
@@ -8,6 +8,9 @@ class TestMakeCons(unittest.TestCase):
 	def testfailintentionally(self):
 		self.assertEqual(1 == 2)
 
+	def testbasicpass(self):
+		self.assertEqual('abc' == 'abc')
+
 
 if __name__ == '__main__':
 	unittest.main()
