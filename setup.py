63c8f298a8279ac4efcc9a061c5f164b54d02514
ozagordi <osvaldo.zagordi+git@gmail.com>
2017-11-09 17:44:29 +0100

    More files
    setuptools files, latex template and vcf used in tests.

--- setup.py
+++ setup.py
@@ -4,33 +4,20 @@ import setuptools
 
 setuptools.setup(
     use_scm_version=True,
-    setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
-    install_requires=['setuptools_scm'],
+    setup_requires=['setuptools_scm', 'setuptools_scm_git_archive',
+                    'isort', 'pytest-runner'],
+    install_requires=['setuptools_scm', 'biopython'],
+    tests_require=['pytest', 'flake8'],
     name='MinVar',
-      description='Minority variants in HIV',
-      url='https://github.com/ozagordi/MinVar',
-      author='Osvaldo Zagordi',
-      author_email='firstname.lastname@gmail.com',
-      packages = setuptools.find_packages(),
-      scripts = ['bin/minvar', 'minvar/blast2sam.py'],
-      data_files = [('minvar/db',
-        ['minvar/db/HXB2_pol_gene.fasta',
-         'minvar/db/HIV_consensus.fasta',
-         'minvar/db/HIV_cons_db.nsq',
-         'minvar/db/HIV_cons_db.nin',
-         'minvar/db/HIV_cons_db.nhr',
-         'minvar/db/template.tex',
-         'minvar/db/masterComments_RTI.txt',
-         'minvar/db/masterComments_PI.txt',
-         'minvar/db/masterComments_INI.txt',
-         'minvar/db/consensus_B.fna',
-         'minvar/db/consensus_B.faa',
-         'minvar/db/consensus_B.bed',
-         'minvar/db/HXB2_pol_gene.fasta.fai',
-         'minvar/db/consensus_B.fna.fai'])],
-      entry_points={
-          'console_scripts': [
-              'minvar = minvar.__main__:main'
-          ]
-    }
+    description='Minority variants in HIV',
+    url='https://github.com/ozagordi/MinVar',
+    author='Osvaldo Zagordi',
+    author_email='firstname.lastname@gmail.com',
+    packages=setuptools.find_packages(),
+    scripts=['bin/minvar', 'src/scripts/blast2sam.py'],
+    package_dir={'minvar': 'src/minvar'},
+    package_data={'minvar': ['db/*']},
+    entry_points={
+        'console_scripts': ['minvar = minvar.__main__:main']
+        }
 )
