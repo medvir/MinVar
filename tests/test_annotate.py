693c0393f914c24102ab364a658908e92fb8ca5f
ozagordi <osvaldo.zagordi+git@gmail.com>
2017-11-15 15:38:05 +0100

    Remove tests of old code

--- tests/test_annotate.py
+++ tests/test_annotate.py
@@ -5,9 +5,7 @@ import os
 import pytest
 from Bio.Seq import Seq
 
-from src.minvar.annotate import (annotate_mutations, extract_hcv_orf,
-                                 find_frame, merge_mutations,
-                                 parse_cons_mutations, parsevar)
+from src.minvar.annotate import (find_frame, merge_mutations, parsevar)
 
 
 subtype1a = '''GCCAGCCCCCTGATGGGGGCGACACTCCACCATGAATCACTCCCCTGTGAGGAACTACTG\
@@ -55,14 +53,6 @@ CAAGCATCGCGTCCTGGGCCATTAAGTGGGAGTACGTCGTTCTCCTGTTCCTTCTGCTTG\
 '''
 
 
-def test_extract_1a():
-    """Identify where orf is: test with genotype 1a"""
-    s, c = extract_hcv_orf(Seq(subtype1a))
-    sta = str(s.translate())
-    assert sta[0] == 'M'
-
-
-# no mismatches
 seq_h77_short = Seq('TTCTTTCACGACCCTGCCAGCCTTGTCCACCGGCCTCATCCACCTCCACCAGAACATTGTGGACGTGCAG')
 # two mismatches
 seq_h77_mism = Seq('TTCTTTCACGACCCTGCCCGCCTTGTCCACCGACCTCATCCACCTCCACCAGAACATTGTGGACGTGCAG')
@@ -70,25 +60,6 @@ seq_h77_mism = Seq('TTCTTTCACGACCCTGCCCGCCTTGTCCACCGACCTCATCCACCTCCACCAGAACATTGT
 seq_h77_ins = Seq('TTCTTTCACGACCCTAAAAAAAAAAGCCAGCCTTGTCCACCGGCCTCATCCACCTCCACCAGAACATTGTGGACGTGCAG')
 # one short deletion, one mismatch
 seq_h77_del = Seq('TTCCACGATGCCAGCCTTGTCCACCGGCCTCATCCACCTCCACCAGAACATTGTGGACGTGCAG')
-
-@pytest.mark.skip(reason="rewritten")
-def test_parse_cons_mutations():
-
-    # test_no_mutations
-    m = parse_cons_mutations(seq_h77_short, 'HCV')
-    assert m.empty
-    # test_two_mismatches
-    m = parse_cons_mutations(seq_h77_mism, 'HCV')
-    assert len(m) == 2
-    # test_one_insertion
-    m = parse_cons_mutations(seq_h77_ins, 'HCV')
-    assert len(m) == 1
-    # test_one_del_one_mism
-    m = parse_cons_mutations(seq_h77_del, 'HCV')
-    assert len(m) == 2
-    assert m.iloc[0].wt == 'T'
-    assert m.iloc[0].mut == 'C'
-
 
 # two silent mutations w.r.t. consensus B
 ref_1 = Seq('TTATCTATCAGTACATGGATGATTTGTATGTAGGATCTGACTTAGAAATTGGGCAGCATAGAACAAAAATAGAGGAACTGAGACAACATCTGTTGAGGTGGGGATTTACCACACCAGACAAAAAACATCAGAAAGAACCTCCATTCCTTT')
@@ -130,7 +101,6 @@ def test_find_frame():
 @pytest.mark.skip(reason='Rewriting this')
 def test_merge_mutations():
     import pandas as pd
-    cm = parse_cons_mutations(ref_1, 'HIV')
     '''
     A, 1011, G
     A, 1050, T
@@ -139,7 +109,7 @@ def test_merge_mutations():
                        ['G', 1011, 'T', 0.2],   # already mutated in sample cons
                        ['T', 1050, 'A', 0.1]],  # mutates back to cons B
                       columns=['wt', 'pos', 'mut', 'freq'])
-    m_out = merge_mutations(cm, vm)
+    m_out = merge_mutations(vm)
     # two mutations in consensus, three in vcf but the one in pos 101 is
     # reverting to consensus B
     assert len(m_out) == 4
@@ -150,24 +120,3 @@ def test_merge_mutations():
     #     with self.subTest(mut=i):
     #         self.assertEqual(out_here.freq, freqs[i])
     #         self.assertEqual(out_here.mut, muts[i])
-
-
-@pytest.mark.skip(reason="check this")
-def test_annotate_mutations():
-    import pandas as pd
-    muts = pd.DataFrame(columns=['gene', 'pos', 'wt', 'mut', 'freq'])
-
-    # no mutation first
-    out = annotate_mutations(muts, ref_3)
-    assert out.empty
-
-    # add one mutation
-    muts = muts.append({'wt': 'T', 'pos': 6, 'mut': 'A', 'freq': 0.2}, ignore_index=True)
-    out = annotate_mutations(muts, ref_3)
-    assert len(out) == 1
-    out_0 = out.iloc[0]
-    assert out_0.pos == 2
-    assert out_0.gene == 'GagPolTF'
-    assert out_0.wt == 'F'
-    assert out_0.mut == 'L'
-    assert out_0.freq == 0.2
